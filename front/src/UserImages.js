0d7a19d140214ed393122ebadab4df53bef46fd4
cstangga <cstangga92@gmail.com>
2024-09-30 15:20:28 +0900

    my-pages->create->swap 이미지 이동

--- front/src/UserImages.js
+++ front/src/UserImages.js
@@ -3,15 +3,19 @@ import { useSelector } from 'react-redux';
 import { storage } from './firebase/config';
 import { ref, listAll, getDownloadURL } from "firebase/storage";
 import './UserImages.css';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link ,useNavigate } from 'react-router-dom';
+
 
 export default function UserImages() {
+    const navigate = useNavigate();  // useNavigate 훅 선언
     const [images, setImages] = useState([]);
+    const [selectedImage, setSelectedImage] = useState(null);
     const [loading, setLoading] = useState(true);
     const user = useSelector(state => state.auth.user);
     const location = useLocation();
     const { from } = location.state || {};
     console.log("from:", from);
+
     useEffect(() => {
         const fetchImages = async () => {
             if (!user || !user.uid) return;
@@ -59,6 +63,11 @@ export default function UserImages() {
         }
     };
 
+    const handleSelectImage = (image) => {
+         // 이미지 선택 후 Create 페이지로 이동
+         navigate('/main/create', { state: { selectedImage: image.url } });
+    };
+
     if (loading) {
         return <div>Loading...</div>;
     }
@@ -79,9 +88,23 @@ export default function UserImages() {
                         />
                         <p>{image.timestamp}: {image.name}</p>
                         <button onClick={() => handleDownload(image)}>다운로드</button>
+                        <button onClick={() => handleSelectImage(image)}>가져가기</button>
                     </div>
                 ))}
             </div>
+
+            {selectedImage && (
+                <div>
+                    {/* 선택된 이미지를 Create 컴포넌트로 전달 */}
+                    <Link 
+                        to="/create"
+                        state={{ selectedImage }} // 선택된 이미지를 상태로 전달
+                        className="go-to-create"
+                    >
+                        선택한 이미지로 이동
+                    </Link>
+                </div>
+            )}
         </div>
     );
 }
